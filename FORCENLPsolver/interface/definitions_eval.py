d8bb2c26deb5f5d2ed785cddcad6e3366c56c22a
lxj595595 <xinjieliu98@163.com>
2022-03-08 15:18:10 +0100

    clean the directories

--- FORCENLPsolver/interface/definitions_eval.py
+++
@@ -1,4 +0,0 @@
-is_static = True
-lib = "libsolver_with_model.so"
-f = "FORCENLPsolver_interface"
-intel_libs_dir = "/home/richard/Applications/Forces_Pro/forces_pro_client/libs_Intel/win64"
--- model/FORCENLPsolver/interface/definitions_eval.py
+++
@@ -1,4 +0,0 @@
-is_static = True
-lib = "solver_with_model.dll"
-f = "FORCENLPsolver_interface"
-intel_libs_dir = "C:\\Users\\Vassil\\Desktop\\Personal\\TU Delft\\Msc Robotics\\forces_pro_client\\libs_Intel\\win64"
