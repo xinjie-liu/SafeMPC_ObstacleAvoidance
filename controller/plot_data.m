639c8c22228c54f2f9c18fb4e0a947083ffd4fb0
Vassil17 <v.v.atanassov@student.tudelft.nl>
2022-03-29 16:38:00 +0200

    Update plot_data.m

--- controller/plot_data.m
+++ controller/plot_data.m
@@ -1,7 +1,9 @@
 %% Compare how N changes the error with terminal cost
 % Plot
+clear;
+clc;
 N = [5,10,20,50];
-xlimit = 900;
+xlimit = 500;
 for trial=1:4
     % Load the data
     load(strcat("experiment_horizon_",num2str(trial),"_data.mat"));
@@ -29,12 +31,14 @@ for trial=1:4
     figure(1);
     hold on;
     xy_error = abs(x_error(1:xlimit)) + abs(y_error(1:xlimit));
-    plot(xy_error(1:xlimit),'DisplayName',"N = "+N(trial));
+    plot(xy_error(1:xlimit),'DisplayName',"N = "+N(trial),'LineWidth',1);
     ylabel('Total absolute error in X and Y')
     xlabel('Timestep')
     legend()
 end
 %% Compare how N changes the error without terminal cost
+clear;
+clc;
 N = [5,10,20,50];
 xlimit = 900;
 for trial=1:4
@@ -43,7 +47,7 @@ for trial=1:4
     figure(4);
     hold on;
     xy_error = abs(x_error(1:xlimit)) + abs(y_error(1:xlimit));
-    plot(xy_error(1:xlimit),'DisplayName',"N = "+N(trial));
+    plot(xy_error(1:xlimit),'DisplayName',"N = "+N(trial),'LineWidth',1);
     ylabel('Total absolute error in X and Y')
     xlabel('Timestep')
     legend()
@@ -51,6 +55,8 @@ for trial=1:4
 end
 %% Compare oldcost vs newcost control input for N=10:
 % Plot control input
+clear;
+clc;
 load(strcat("experiment_uref_",num2str(1),"_data.mat"));
 xlimit = 900;
 trial = 2;
@@ -58,7 +64,7 @@ load(strcat("experiment_newcost_control_",num2str(trial),"_data.mat"));
 figure(1);
 hold on;
 control_input = sum(abs(uStore-uref(1:990,:)),2);
-plot(control_input(1:xlimit),'DisplayName',"Control input with Terminal cost");
+plot(control_input(1:xlimit),'DisplayName',"Control input with Terminal cost",'LineWidth',1);
 ylabel('Total control input')
 xlabel('Timestep')
 legend()
@@ -66,15 +72,17 @@ legend()
 load(strcat("experiment_oldcost_control_",num2str(trial),"_data.mat"));
 figure(1);
 control_input = sum(abs(uStore-uref(1:990,:)),2);
-plot(control_input(1:xlimit),'DisplayName',"Control input without Terminal cost");
+plot(control_input(1:xlimit),'DisplayName',"Control input without Terminal cost",'LineWidth',1);
 ylabel('Total control input')
 xlabel('Timestep')
 legend()
 
-%% For comparison between with terminal cost and without:
+%% For comparison between with terminal cost and without N=10:
 % Plot
+clear;
+clc;
 label = {'With Terminal Cost';'Without Terminal Cost'};
-for trial=1:2
+for trial=1:2 
     % Load the data
     load(strcat("experiment_",num2str(trial),"_data.mat"));
 %     % Plot x axis error
@@ -101,7 +109,7 @@ for trial=1:2
     figure(4);
     hold on;
     xy_error = abs(x_error(1:900)) + abs(y_error(1:900));
-    plot(xy_error(1:900),'DisplayName',label{trial});
+    plot(xy_error(1:900),'DisplayName',label{trial},'LineWidth',1);
     ylabel('Total absolute error in X and Y')
     xlabel('Timestep')
     legend()
@@ -118,7 +126,7 @@ for trial=1:4
     figure(1);
     hold on;
     xy_error = abs(x_error(1:xlimit)) + abs(y_error(1:xlimit));
-    plot(xy_error(1:xlimit),'DisplayName',strcat("Total error in X and Y for beta = ",num2str(beta(trial))));
+    plot(xy_error(1:xlimit),'DisplayName',strcat("Total error in X and Y for beta = ",num2str(beta(trial))),'LineWidth',1);
     ylabel('Total absolute error in X and Y')
     xlabel('Timestep')
     legend()
@@ -126,7 +134,7 @@ for trial=1:4
     figure(2);
     hold on;
     control_input = sum(abs(uStore-uref(1:990,:)),2);
-    plot(control_input(1:xlimit),'DisplayName',strcat("Control input for beta = ",num2str(beta(trial))));
+    plot(control_input(1:xlimit),'DisplayName',strcat("Control input for beta = ",num2str(beta(trial))),'LineWidth',1);
     ylabel('Total control input')
     xlabel('Timestep')
     legend()
@@ -138,18 +146,25 @@ clc;
 xlimit = 900;
 load(strcat("experiment_LQR_",num2str(1),"_data.mat"));
 figure(1);
-hold on;
 control_input = (uStore-uref(1:990,:));
-plot(control_input(1:xlimit,1),'DisplayName',"Control input v for LQR");
-plot(control_input(1:xlimit,2),'DisplayName',"Control input w for LQR");
+subplot(2,1,1);
+hold on
+plot(control_input(1:xlimit,1),'DisplayName',"Control input v for LQR",'LineWidth',1);
+legend()
+subplot(2,1,2);
+hold on
+plot(control_input(1:xlimit,2),'DisplayName',"Control input w for LQR",'LineWidth',1);
 ylabel('Total control input')
 xlabel('Timestep')
 legend()
 % N = 10
-load(strcat("experiment_LQR_",num2str(4),"_data.mat"));
+load(strcat("experiment_LQR_",num2str(2),"_data.mat"));
 control_input = (uStore-uref(1:990,:));
-plot(control_input(1:xlimit,1),'DisplayName',"Control input v for MPC");
-plot(control_input(1:xlimit,2),'DisplayName',"Control input w for MPC");
+subplot(2,1,1);
+plot(control_input(1:xlimit,1),'DisplayName',"Control input v for MPC",'LineWidth',1);
+legend()
+subplot(2,1,2);
+plot(control_input(1:xlimit,2),'DisplayName',"Control input w for MPC",'LineWidth',1);
 ylabel('Total control input')
 xlabel('Timestep')
 legend()
