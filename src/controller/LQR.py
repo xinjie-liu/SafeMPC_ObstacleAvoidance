eae3421b829f59ae4f72d84ad822b5e92eb4c542
xinjie-liu <xinjieliu98@163.com>
2024-06-23 13:06:14 -0500

    add folder

--- src/controller/LQR.py
+++ src/controller/LQR.py
@@ -2,7 +2,8 @@
 File containing the class definition of the unconstrained LQR controller. It solves the discrete algebraic
 Riccati equation and uses that K to follow the trajectory. Run this script if you want to see the performance.
 """
-
+import os, sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../'))
 import numpy as np
 import matplotlib.pyplot as plt
 import mpl_toolkits.mplot3d.axes3d as p3
